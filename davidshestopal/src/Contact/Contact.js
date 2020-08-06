f553fcb48323ea2240bd018bf9bd99b2fae24fd7
David Shestopal <dshestopal@yahoo.com>
2020-08-06 10:24:15 -0700

    contact page created and styled

--- davidshestopal/src/Contact/Contact.js
+++ davidshestopal/src/Contact/Contact.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import ContactInfo from './ContactInfo';
+import Hero from '../components/Hero';
 
-export default function Contact() {
-  return (
-    <div>
-      <p>contact</p>
-    </div>
-  );
+class ContactPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <ContactInfo />
+      </div>
+    );
+  }
 }
+
+export default ContactPage;
---
+++ davidshestopal/src/Contact/ContactInfo.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import {
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardDeck,
+  CardSubtitle,
+  Jumbotron,
+  Container,
+  CardBody,
+} from 'reactstrap';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import MailIcon from '@material-ui/icons/Mail';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Divider from '@material-ui/core/Divider';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 760,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function InsetDividers() {
+  const classes = useStyles();
+
+  return (
+    <CardDeck style={{ marginLeft: '1rem', marginRight: '1rem', marginTop: '2rem', marginBottom: '4rem' }}>
+      <Card style={{ border: 'none' }}>
+        <CardBody>
+          <div>
+            <Jumbotron fluid style={{ background: 'none' }}>
+              <Container fluid>
+                <h1 className="display-1">Let's Talk</h1>
+                <p className="lead">Please feel free to reach out to me and let's make your dreams a reality!</p>
+              </Container>
+            </Jumbotron>
+          </div>
+        </CardBody>
+      </Card>
+
+      <Card style={{}}>
+        <CardBody>
+          <List className={classes.root} style={{ marginLeft: '1rem' }}>
+            <ListItem>
+              <ListItemAvatar>
+                <a href="https://www.linkedin.com/in/david-shestopal-4154731a1">
+                  <LinkedInIcon style={{ fontSize: '100px', marginRight: '2rem' }} />
+                </a>
+              </ListItemAvatar>
+              <a href="https://www.linkedin.com/in/david-shestopal-4154731a1">
+                <p style={{ fontSize: '2rem' }}>LinkedIn</p>
+              </a>
+            </ListItem>
+            <hr />
+
+            <ListItem>
+              <ListItemAvatar>
+                <a href="mailto:dshestopal@yahoo.com">
+                  <MailIcon style={{ fontSize: '100px', paddingRight: '1rem', marginRight: '2rem' }} />
+                </a>
+              </ListItemAvatar>
+              <a href="mailto:dshestopal@yahoo.com">
+                <p style={{ fontSize: '2rem' }}>Email</p>
+              </a>
+            </ListItem>
+            <hr />
+            <ListItem>
+              <ListItemAvatar>
+                <a href="https://github.com/DavidShestopal">
+                  {' '}
+                  <GitHubIcon style={{ fontSize: '100px', paddingRight: '1rem', marginRight: '2rem' }} />
+                </a>
+              </ListItemAvatar>
+              <a href="https://github.com/DavidShestopal">
+                {' '}
+                <p style={{ fontSize: '2rem' }}>Github</p>
+              </a>
+            </ListItem>
+          </List>
+        </CardBody>
+      </Card>
+    </CardDeck>
+  );
+}
